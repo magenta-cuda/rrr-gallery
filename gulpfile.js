cebe36a2ea7b19dd5e2ead21338f93dfb5e52a2a
Magenta Cuda <magenta.cuda@yahoo.com>
2019-06-26 17:28:12 -1000

    add gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp       = require('gulp');
 var sass       = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var uglify     = require('gulp-uglify');
+var rename     = require('gulp-rename');
 
 gulp.task('sass', function(){
     return gulp.src('css/*.scss')
@@ -8,4 +10,11 @@ gulp.task('sass', function(){
         .pipe(sass())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('css'))
+});
+
+gulp.task('js', function(){
+    return gulp.src(['js/*.js', '!js/*.min.js'])
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('js'))
 });
