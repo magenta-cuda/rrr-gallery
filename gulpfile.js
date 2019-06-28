8abdc157546d9ac14d26fa1a348ca06189f8b708
Magenta Cuda <magenta.cuda@yahoo.com>
2019-06-28 00:06:08 -1000

    fix js.min.map bug in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp       = require('gulp');
 var sass       = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify     = require('gulp-uglify');
+var gulpif     = require('gulp-if');
 var rename     = require('gulp-rename');
 
 gulp.task('sass', function(){
@@ -10,15 +11,17 @@ gulp.task('sass', function(){
         .pipe(sass())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('css'))
+        .pipe(gulpif('*.css',gulp.dest('dist/css')))
 });
 
 gulp.task('js', function(){
-    return gulp.src(['js/*.js', '!js/*.min.js'])
+    return gulp.src(['js/*.js', '!js/*.min.js', '!js/bbg_xiv-gallery-extra.js'])
         .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(sourcemaps.write('.'))
-        .pipe(rename({suffix: '.min'}))
+        .pipe(gulpif('*.js', rename({suffix:'.min'})))
         .pipe(gulp.dest('js'))
+        .pipe(gulpif('*.min.js',gulp.dest('dist/js')))
 });
 
 gulp.task('watch', function(){
