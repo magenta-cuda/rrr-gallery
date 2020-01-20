49a656f0bb4cf7615a68ccc1bf99963f79418231
Magenta Cuda <magenta.cuda@yahoo.com>
2020-01-20 05:16:24 -1000

    debugging proxy for the Redux reducer

--- js/rr-main.js
+++ js/rr-main.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom'
 import {createStore, applyMiddleware, compose} from 'redux'
 import { connect } from 'react-redux'
 import thunk from 'redux-thunk'
-import { createLogger } from 'redux-logger'
+// import { createLogger } from 'redux-logger'
 import { Provider } from 'react-redux'
-import reducer from './reducers/index.js'
+// import reducer from './reducers/index.js'
+// proxy.js is a lightweight debugging wrapper for the Redux reducer
+import reducer from './reducers/proxy.js'
 import start from './middleware/start.js'
 import rest from './middleware/rest.js'
 import postRest from './middleware/postRest.js'
 import end from './middleware/end.js'
 import Frame from './containers/Frame.js'
-//import NavBar from './containers/NavBar.js'
-import DevTools from './containers/DevTools.js'
+// import NavBar from './containers/NavBar.js'
+// import DevTools from './containers/DevTools.js'
 import {getImagesByGallerySpecs, getImagesBySearchParms, loadGalleryImages, setView, toggleFullScreen, toggleCaptions,
         setQuery, setConfiguration} from './actions/index.js'
 import {CssReducer, JqueryProxy} from './CssReducer.js'
@@ -23,7 +25,8 @@ var debug        = searchParams.has("mc_debug")
 debug && console.log("rr-main.js loading...")
 
 const store = createStore(reducer, {galleries: {images: {}}, configuration: {}},
-                          compose(applyMiddleware( thunk, start, rest, postRest, end, createLogger()), DevTools.instrument()))
+                          // compose(applyMiddleware(thunk, start, rest, postRest, end, createLogger()), DevTools.instrument()))
+                          applyMiddleware(thunk, start, rest, postRest, end))
 
 // TODO: need store in global scope for now to do testing; remove for production.
 // TODO: need actions in global scope for now as cannot import yet; remove for production.
