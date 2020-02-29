536249a132c4c642ed7fce6972e0cf23583cccee
Magenta Cuda <magenta.cuda@yahoo.com>
2020-02-29 08:32:47 -1000

    shouldn't save the show property in configuration to the cookie

--- js/middleware/cookie.js
+++ js/middleware/cookie.js
@@ -1,16 +1,16 @@
+// Since configuration is per user it needs to be persisted on the client side in a cookie or in local storage
+
 import {SET_CONFIGURATION} from '../actions/index.js'
 
 export default store => next => action => {
-    console.log("%%%%%:middleware:SET_CONFIGURATION::action=", action)
     if (action.type === SET_CONFIGURATION) {
         let cookie = bbg_xiv.getCookie("bbg_xiv")
         cookie = cookie ? JSON.parse(cookie) : {}
-        cookie = Object.assign(cookie, action.configuration) 
+        cookie = Object.assign(cookie, action.configuration)
+        delete cookie.show
         cookie = JSON.stringify(cookie)
-        console.log("%%%%%:middleware:SET_CONFIGURATION::cookie=", cookie)
         bbg_xiv.setCookie("bbg_xiv", cookie, 30)
     }
     const ret = next(action)
-    console.log("%%%%%:middleware:SET_CONFIGURATION::ret=", ret)
     return ret
 }
